44f1bc1d1ee3d2277247660f919383648de30ba3
Tiago Hermano <tiagofhermano@gmail.com>
2018-01-24 12:25:19 -0200

    Init projeto spotify-clone

---
+++ spotify-clone/src/index.js
@@ -0,0 +1,10 @@
+import 'config/ReactotronConfig';
+
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => (
+  <View />
+);
+
+export default App;
---
+++ spotify-clone/src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  background: '#191919',
+  success: '#9DCA83',
+  danger: '#E37A7A',
+};
