15bad1f0407b3bdc16243d82e4039337bebcf29a
Tiago Hermano <tiagofhermano@gmail.com>
2018-01-31 12:26:23 -0200

    Criado novo teste utilizando metodologia TDD

--- testsrn/__tests__/components/list.test.js
+++ testsrn/__tests__/components/list.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import List from '../../src/List';
 
-import { Text } from 'react-native';
+import { Text, ScrollView, Button } from 'react-native';
 
 const posts = [
   {id: 1, title: 'Teste', description: 'Descrição'},
@@ -13,15 +13,26 @@ const posts = [
 
 describe('Testing List', () => {
   it('renders as expected', () => {
-    const wrapper =  shallow(<List posts={posts} />);
-    expect(wrapper.children()).toHaveLength(3);
+    const wrapper =  shallow(<List />);
+    expect(wrapper.find(ScrollView).exists()).toBe(false);
+
+    wrapper.setState({ posts });
+    expect(wrapper.find(ScrollView).children()).toHaveLength(3);
+  });
+
+  it('can add new post', () => {
+    const wrapper =  shallow(<List />);
+
+    wrapper.find(Button).simulate('press');
+
+    expect(wrapper.state('posts')).toHaveLength(1);
   });
 
   it('shows empty message', () => {
-    const wrapper =  shallow(<List posts={[]} />);
+    const wrapper =  shallow(<List />);
     expect(wrapper.contains(<Text>Nenhum post</Text>)).toBe(true);
 
     wrapper.setProps({ posts });
-    expect(wrapper.contains(<Text>Nenhum post</Text>)).toBe(false);
+    expect(wrapper.contains(<Text>Nenhum post</Text>)).toBe(true);
   });
 });
--- testsrn/src/List.js
+++ testsrn/src/List.js
@@ -1,24 +1,48 @@
 /* @flow weak */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
+  ScrollView,
   Text,
+  Button,
   StyleSheet,
 } from 'react-native';
 
 import Post from './Post';
 
-const List = ({ posts }) => (
-  <View style={styles.container}>
-    { posts.length > 0
-      ? posts.map(post => <Post key={post.id} post={post} />)
-      : <Text>Nenhum post</Text>
-    }
-  </View>
-);
+export default class List extends Component {
+  state = {
+    posts: [],
+  }
 
-export default List;
+  renderPosts = () => (
+    <ScrollView>
+      { this.state.posts.map(post => <Post key={post.id} post={post} />) }
+    </ScrollView>
+  )
+
+  addPost = () => {
+    this.setState({
+      posts: [
+        ...this.state.posts,
+        { id: Math.random(), title: 'Post Aleatório', description: 'Teste' }
+      ],
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        { this.state.posts.length > 0
+          ? this.renderPosts()
+          : <Text>Nenhum post</Text>
+        }
+        <Button title="Add post" onPress={this.addPost} />
+      </View>
+    )
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
